1d8ca4923bf1b64f40cb5fa85621ec329b7d2a55
root <root@DESKTOP-FHP8M5J>
2025-01-14 14:42:32 +0100

    worked some bit on CSS

--- frontend/Ikigai/src/components/Welcome/Welcome.jsx
+++ frontend/Ikigai/src/components/Welcome/Welcome.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
+import styles from "./Welcome.module.css";
 
 
 function Welcome (){
     const navigate = useNavigate();
 
     return(
-        <div className="container">
-            <div className="frame">
-                <div className="top-branch"><img src="" alt="top-branch" /></div>
-                <h4>Ikiga-i</h4>
-                <h1>Welcome to your ikigai journey</h1>
-                <button onClick={()=> navigate('/ikigaiDescription')}>start</button>
-                <div className="lower-branch"><img src="" alt="lower-branch" /></div>
+        <div className={styles.container}>
+            <h4 className={styles.title}>Ikiga-i</h4>
+            <div className={styles.frame}>
+                {/* <div className={styles.top-branch}><img src="" alt="top-branch" /></div> */}
+                <h1 className={styles.subtitle}>Welcome to your ikigai journey</h1>
+                <button className={styles.startButton} onClick={()=> navigate('/ikigaiDescription')}>start</button>
+                {/* <div className={styles.lower-branch}><img src="" alt="lower-branch" /></div> */}
             </div>
-            <div className="team">DEVELOPED BY TEAM ZEN</div>
+            <div className={styles.team}>DEVELOPED BY TEAM ZEN</div>
         </div>
     )
 }
