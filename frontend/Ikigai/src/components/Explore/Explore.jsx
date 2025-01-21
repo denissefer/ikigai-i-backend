a4783efb5bdb29f8a4a7ad0be202875d8a5016e1
root <root@DESKTOP-FHP8M5J>
2025-01-22 00:39:12 +0100

    the map still needs some changes

--- frontend/Ikigai/src/components/Explore/Explore.jsx
+++ frontend/Ikigai/src/components/Explore/Explore.jsx
@@ -4,18 +4,32 @@ import styles from "./Explore.module.css"
 
 
 export default function Explore() {
-
+    const navigate = useNavigate();
   return (
     <div className={styles.main_container}>
-      <div className={styles.title}></div>
-      <div className={styles.japanese_concepts}>
-        <div className={styles.card_container}>
-          <div className={styles.section_name}></div>
-          <div className={styles.image}></div>
-          <div className={styles.concept_name}></div>
+      <div className={styles.title}>
+        <div className={styles.japanese_concepts}>
+          <div className={styles.card_container}>
+            <div className={styles.section_name}></div>
+            <div className={styles.image}></div>
+            <div className={styles.concept_name}></div>
+          </div>
+          <span>flip</span> <br />
+          <span>swipe</span>
         </div>
-        <span>flip</span>
-        <span>swipe</span>
+        <div className={styles.inspirations}>
+          <span>books</span> <br />
+          <span>movies</span>
+        </div>
+        <div className={styles.inspirations}>My Bucket List</div>
+        <div className={styles.inspirations}>3-Months-Planner</div>
+      </div>
+      <div className={styles.navigation}>
+        <ul>
+          <li>explore</li>
+          <li>ikigai map</li>
+          <li>me</li>
+        </ul>
       </div>
     </div>
   )
